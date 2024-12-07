370d86d9fd8f2f1bbf40fc819b07cbffeaee3f80
happycodez <happybrown2k@gmail.com>
2024-12-07 18:35:23 +0200

    user error

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -19,7 +19,7 @@ const LoginModal = ({
 }) => {
     return (
         <Dialog onOpenChange={setIsOpen} open={isOpen}>
-            <DialogContent className='absolute z-[9999999]'>
+            <DialogContent className='absolute z-[99999] mt-10'>
                 <DialogHeader>
                     <div className='relative mx-auto w-24 h-24 mb-2'>
                         <Image
