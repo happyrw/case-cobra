03e2904439b0b83b439cdd3e82c711a44565bb5f
happycodez <happybrown2k@gmail.com>
2024-12-08 09:18:27 +0200

    middleware added

--- next.config.ts
+++ next.config.ts
@@ -3,31 +3,31 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["utfs.io"],
-  },
-  async headers() {
-    return [
-      {
-        // Match all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://case-cobra-psi.vercel.app" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      },
-      {
-        // Handle OPTIONS requests
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://case-cobra-psi.vercel.app" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-        ]
-      }
-    ];
   }
+  // async headers() {
+  //   return [
+  //     {
+  //       // Match all API routes
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "https://case-cobra-psi.vercel.app" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  //       ]
+  //     },
+  //     {
+  //       // Handle OPTIONS requests
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Origin", value: "https://case-cobra-psi.vercel.app" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //       ]
+  //     }
+  //   ];
+  // }
 };
 
 export default nextConfig;
